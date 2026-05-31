6a45fa61d1e5a7af28c3c266a9d01a0faa354b74
LucStk <luc.sterkers@laposte.net>
2026-05-31 18:07:22 +0200

    test création waypoint onMapTap

--- vamos_cartographie/lib/features/map/presentation/widgets/map_view.dart
+++ vamos_cartographie/lib/features/map/presentation/widgets/map_view.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:vamos_cartographie/features/features.dart';
 import 'package:flutter_map_dragmarker/flutter_map_dragmarker.dart';
 import 'package:vamos_cartographie/features/map/application/applications.dart';
+import 'package:vamos_cartographie/features/map/presentation/dialogs/waypoint_creator_dialog.dart';
 import 'package:vamos_cartographie/features/map/presentation/layers/layers.dart';
 import 'package:vamos_cartographie/features/map/presentation/dialogs/dialogs.dart';
-import 'map_context_menu.dart';
 
 class MapView extends ConsumerStatefulWidget {
   const MapView({super.key});
@@ -30,8 +29,13 @@ class _MapViewState extends ConsumerState<MapView> {
     _mapController = MapController();
   }
 
-  void _onMapTap(LatLng latLng) {
-    _mapNotifier.startWaypointCreation(latLng);
+  void _onMapTap(BuildContext context, LatLng latLng) {
+    // _mapNotifier.startWaypointCreation(latLng);
+    WaypointCreatorDialog.show(
+      context: context,
+      latLng: latLng,
+      tripId: _tripId,
+    );
   }
 
   void _showWaypoint(int waypointId) {
@@ -47,10 +51,10 @@ class _MapViewState extends ConsumerState<MapView> {
       none: (_) => [],
 
       creatingWaypoint: (interaction) => [
-        MapContextMenu(
-          position: interaction.position,
-          onClose: _mapNotifier.cancelInteraction,
-        ),
+        // MapContextMenu(
+        //   position: interaction.position,
+        //   onClose: _mapNotifier.cancelInteraction,
+        // ),
       ],
 
       creatingSegment: (_) => [],
@@ -66,7 +70,7 @@ class _MapViewState extends ConsumerState<MapView> {
       options: MapOptions(
         initialCenter: const LatLng(46.8, 2.2),
         initialZoom: 7,
-        onTap: (_, latLng) => _onMapTap(latLng),
+        onTap: (_, latLng) => _onMapTap(context, latLng),
       ),
       children: [
         MapTileLayer(),
