f4405f1068ea29efed7a4903eebaefc703ffbc83
LucStk <luc.sterkers@laposte.net>
2026-06-05 19:15:24 +0200

    Update main.dart

--- vamos_cartographie/lib/main.dart
+++ vamos_cartographie/lib/main.dart
@@ -2,15 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vamos_cartographie/app.dart';
-import 'package:vamos_cartographie/core/injection.dart'; // Ton fichier où se trouve configureDependencies
+import 'package:vamos_cartographie/core/injection/client_provider.dart'; // Ton fichier où se trouve configureDependencies
+import 'package:vamos_cartographie/core/network/graphql/ferry_client.dart';
 
-Future<void> main() async {
-  // 1. Obligatoire si tu fais du async avant runApp (ex: GetIt, SharedPreferences, etc.)
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   debugProfileBuildsEnabled = true;
-  // 2. On initialise GetIt (chargement de tes repositories, dépendances...)
-  await configureDependencies();
 
-  // 3. On lance l'application enveloppée dans le ProviderScope de Riverpod
-  runApp(const ProviderScope(child: CartographieApp()));
+  final client = initFerryClient('http://localhost:8000/graphql/');
+
+  runApp(
+    ProviderScope(
+      overrides: [clientProvider.overrideWithValue(client)],
+      child: CartographieApp(),
+    ),
+  );
 }
