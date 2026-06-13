c78ac8fe88fec5f3252722a7f2f697e8cc94b9f2
LucStk <luc.sterkers@laposte.net>
2026-06-13 18:37:31 +0200

    clean

--- vamos_cartographie/lib/features/map/presentation/layers/topology/topology_layer.dart
+++ vamos_cartographie/lib/features/map/presentation/layers/topology/topology_layer.dart
@@ -9,31 +9,18 @@ import "segment_type_marker_layer.dart";
 
 class TopologyLayer extends ConsumerWidget {
   // 1. On hérite de ConsumerWidget
-  final Id<Trip>
-  tripId; // Récupéré de ton besoin initial (ou via super si AbstractLayer le stocke)
-
+  final Id<Trip> tripId;
   const TopologyLayer({super.key, required this.tripId});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // 2. La méthode build est synchrone
-
-    // 3. On "watch" le provider asynchrone en lui passant l'ID
     final tripGraphAsync = ref.watch(tripGraphProvider(tripId));
 
-    // 4. On utilise .when pour gérer le cycle de vie asynchrone
     return tripGraphAsync.when(
-      loading: () => const Center(
-        child:
-            CircularProgressIndicator(), // Écran de chargement pendant que le loader tourne
-      ),
-      error: (err, stack) => Center(
-        child: Text(
-          'Erreur lors du chargement du graphe : $err',
-        ), // Gestion des erreurs (.fold de tes repos)
-      ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) =>
+          Center(child: Text('Erreur lors du chargement du graphe : $err')),
       data: (graphStore) {
-        // Le graphe est chargé et prêt ! On affiche les sous-layers
         return Stack(
           children: [
             SegmentLayer(tripId: tripId),
