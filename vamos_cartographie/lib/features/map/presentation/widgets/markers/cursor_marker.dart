82f96d4be87dd92a0f75920999c380cb8e0a8119
LucStk <luc.sterkers@laposte.net>
2026-06-15 17:46:03 +0200

    rework cursor style

--- vamos_cartographie/lib/features/map/presentation/widgets/markers/cursor_marker.dart
+++ vamos_cartographie/lib/features/map/presentation/widgets/markers/cursor_marker.dart
@@ -8,18 +8,19 @@ class CursorMarker extends MapMarker {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(color: Colors.white, width: 2),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Icon(Icons.circle, size: 8, color: Colors.white.withOpacity(0.5)),
+      alignment: Alignment.topCenter,
+      // decoration: BoxDecoration(
+      //   shape: BoxShape.circle,
+      //   border: Border.all(color: Colors.white, width: 2),
+      //   boxShadow: [
+      //     BoxShadow(
+      //       color: Colors.black.withOpacity(0.2),
+      //       blurRadius: 4,
+      //       offset: const Offset(0, 2),
+      //     ),
+      //   ],
+      // ),
+      child: Icon(Icons.place_sharp, size: 30, color: Colors.black),
     );
   }
 }
--- vamos_cartographie/lib/features/map/presentation/widgets/menus/cursor_menu.dart
+++ vamos_cartographie/lib/features/map/presentation/widgets/menus/cursor_menu.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vamos_cartographie/features/map/application/providers/cursor_provider.dart';
 import 'package:vamos_cartographie/features/map/presentation/widgets/buttons/create_waypoint_button.dart';
 import 'package:vamos_cartographie/features/map/presentation/widgets/menus/pop_menu_shell.dart';
+import 'package:vamos_cartographie/features/waypoints/application/orchestrators/waypoint_orchestrator.dart';
+import 'package:vamos_cartographie/features/waypoints/presentation/dialogs/waypoint_creator_dialog.dart';
 import "pop_menu.dart";
 
 class CursorMenu extends PopMenu {
@@ -10,11 +12,17 @@ class CursorMenu extends PopMenu {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final cursor = ref.watch(mapCursorProvider);
     final cursorNotifier = ref.watch(mapCursorProvider.notifier);
     return PopMenuShell(
       buttonsBuilder: (ctx) => [
         CreateWaypointButton(
-          onPressed: () {
+          onPressed: () async {
+            WaypointCreatorDialog.show(
+              context: context,
+              tripId: tripId,
+              latLng: cursor.latLng,
+            );
             cursorNotifier.close();
           },
         ),
