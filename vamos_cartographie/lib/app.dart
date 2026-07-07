4d4ed9342ad0c5d46ceac491f8bb44bd7d14a0f3
LucStk <luc.sterkers@laposte.net>
2026-07-07 09:35:52 +0200

    notificationWidget par dessus tous les pages

--- vamos_cartographie/lib/app.dart
+++ vamos_cartographie/lib/app.dart
@@ -3,17 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:vamos_cartographie/features/notifications/widgets/notification.dart';
 import 'package:vamos_cartographie/features/trips/screens/explorer_page.dart';
 
-class AppShell extends StatelessWidget {
-  const AppShell({super.key, required this.child});
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return NotificationListenerWidget(child: child);
-  }
-}
-
 class CartographieApp extends StatelessWidget {
   const CartographieApp({super.key});
 
@@ -29,7 +18,8 @@ class CartographieApp extends StatelessWidget {
       ],
       supportedLocales: const [Locale('fr', 'FR'), Locale('en', 'US')],
       locale: const Locale('fr', 'FR'),
-      home: const AppShell(child: ExplorerPage()),
+      builder: (context, child) => NotificationListenerWidget(child: child!),
+      home: const ExplorerPage(),
     );
   }
 }
