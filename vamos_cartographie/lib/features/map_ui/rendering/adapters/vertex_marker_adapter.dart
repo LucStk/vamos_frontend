9d83d338143bcc77047117afc469fa43eb4f6556
LucStk <luc.sterkers@laposte.net>
2026-07-09 09:24:41 +0200

    séparation des widgets connection_handle

--- vamos_cartographie/lib/features/map_ui/rendering/adapters/vertex_marker_adapter.dart
+++ vamos_cartographie/lib/features/map_ui/rendering/adapters/vertex_marker_adapter.dart
@@ -5,6 +5,7 @@ import 'package:trip_domain/domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:vamos_cartographie/core/injection/map_state_provider.dart';
 import 'package:vamos_cartographie/features/map_ui/rendering/elements/vertex_ui_element.dart';
+import 'package:vamos_cartographie/features/map_ui/rendering/widgets/connection_handle.dart';
 
 DragMarker toVertexMarker(
   VertexUiElement element,
@@ -34,47 +35,21 @@ DragMarker toVertexMarker(
 
           Positioned(
             left: -handleSize * 0.6,
-            child: _ConnectionHandle(
+            child: ConnectionHandle(
               type: ConnectionHandleType.incoming,
+              element: element,
+              mapStateNotifier: mapStateNotifier,
               size: handleSize,
-              onPanStart: (details) => mapStateNotifier.sendUiEvent(
-                element.connectionDragStartEvent(
-                  ConnectionHandleType.incoming,
-                  details.globalPosition,
-                ),
-              ),
-              onPanUpdate: (details) => mapStateNotifier.sendUiEvent(
-                element.connectionDragUpdateEvent(
-                  ConnectionHandleType.incoming,
-                  details.globalPosition,
-                ),
-              ),
-              onPanEnd: (_) => mapStateNotifier.sendUiEvent(
-                element.connectionDragEndEvent(ConnectionHandleType.incoming),
-              ),
             ),
           ),
 
           Positioned(
             right: -handleSize * 0.6,
-            child: _ConnectionHandle(
+            child: ConnectionHandle(
               type: ConnectionHandleType.outgoing,
+              element: element,
+              mapStateNotifier: mapStateNotifier,
               size: handleSize,
-              onPanStart: (details) => mapStateNotifier.sendUiEvent(
-                element.connectionDragStartEvent(
-                  ConnectionHandleType.outgoing,
-                  details.globalPosition,
-                ),
-              ),
-              onPanUpdate: (details) => mapStateNotifier.sendUiEvent(
-                element.connectionDragUpdateEvent(
-                  ConnectionHandleType.outgoing,
-                  details.globalPosition,
-                ),
-              ),
-              onPanEnd: (_) => mapStateNotifier.sendUiEvent(
-                element.connectionDragEndEvent(ConnectionHandleType.outgoing),
-              ),
             ),
           ),
         ],
@@ -85,57 +60,4 @@ DragMarker toVertexMarker(
     onDragEnd: (_, LatLng latLng) =>
         mapStateNotifier.sendUiEvent(element.dragEndEvent(latLng)),
   );
-}
-
-class _ConnectionHandle extends StatelessWidget {
-  const _ConnectionHandle({
-    required this.type,
-    required this.size,
-    required this.onPanStart,
-    required this.onPanUpdate,
-    required this.onPanEnd,
-  });
-
-  final ConnectionHandleType type;
-  final double size;
-  final GestureDragStartCallback onPanStart;
-  final GestureDragUpdateCallback onPanUpdate;
-  final GestureDragEndCallback onPanEnd;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onPanStart: onPanStart,
-      onPanUpdate: onPanUpdate,
-      onPanEnd: onPanEnd,
-      // Zone tactile plus large (~44px) que le rendu visuel
-      child: SizedBox(
-        width: size * 2,
-        height: size * 2,
-        child: Center(
-          child: Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              border: Border.all(
-                color: type == ConnectionHandleType.incoming
-                    ? Colors.blue
-                    : Colors.green,
-                width: 2,
-              ),
-            ),
-            child: Icon(
-              type == ConnectionHandleType.incoming
-                  ? Icons.arrow_back
-                  : Icons.arrow_forward,
-              size: size * 0.6,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
