594781766bcf088cc75e081cf04ec911a11f7398
LucStk <luc.sterkers@laposte.net>
2026-07-09 19:42:08 +0200

    providers en place

---
+++ vamos_cartographie/lib/core/injection/topology/providers/vertex_provider.g.dart
@@ -0,0 +1,201 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vertex_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(vertexRemoteDatasource)
+final vertexRemoteDatasourceProvider = VertexRemoteDatasourceProvider._();
+
+final class VertexRemoteDatasourceProvider
+    extends
+        $FunctionalProvider<
+          VertexRemoteDatasource,
+          VertexRemoteDatasource,
+          VertexRemoteDatasource
+        >
+    with $Provider<VertexRemoteDatasource> {
+  VertexRemoteDatasourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'vertexRemoteDatasourceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexRemoteDatasourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<VertexRemoteDatasource> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  VertexRemoteDatasource create(Ref ref) {
+    return vertexRemoteDatasource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(VertexRemoteDatasource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<VertexRemoteDatasource>(value),
+    );
+  }
+}
+
+String _$vertexRemoteDatasourceHash() =>
+    r'06a82eb656f73e16ff72176824f7c2cb28c87374';
+
+@ProviderFor(vertexRepository)
+final vertexRepositoryProvider = VertexRepositoryProvider._();
+
+final class VertexRepositoryProvider
+    extends
+        $FunctionalProvider<
+          VertexRepository,
+          VertexRepository,
+          VertexRepository
+        >
+    with $Provider<VertexRepository> {
+  VertexRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'vertexRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<VertexRepository> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  VertexRepository create(Ref ref) {
+    return vertexRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(VertexRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<VertexRepository>(value),
+    );
+  }
+}
+
+String _$vertexRepositoryHash() => r'66dd891c01a4ecc21944f43123c221426aaebe65';
+
+@ProviderFor(vertexStore)
+final vertexStoreProvider = VertexStoreProvider._();
+
+final class VertexStoreProvider
+    extends
+        $FunctionalProvider<
+          ObservableCollectionStore<Vertex>,
+          ObservableCollectionStore<Vertex>,
+          ObservableCollectionStore<Vertex>
+        >
+    with $Provider<ObservableCollectionStore<Vertex>> {
+  VertexStoreProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'vertexStoreProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexStoreHash();
+
+  @$internal
+  @override
+  $ProviderElement<ObservableCollectionStore<Vertex>> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ObservableCollectionStore<Vertex> create(Ref ref) {
+    return vertexStore(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ObservableCollectionStore<Vertex> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ObservableCollectionStore<Vertex>>(
+        value,
+      ),
+    );
+  }
+}
+
+String _$vertexStoreHash() => r'4fa740f67a2c82102da5ecd9433e6cd261aef786';
+
+@ProviderFor(vertexPatchStore)
+final vertexPatchStoreProvider = VertexPatchStoreProvider._();
+
+final class VertexPatchStoreProvider
+    extends
+        $FunctionalProvider<
+          ObservableCollectionStore<VertexPatch>,
+          ObservableCollectionStore<VertexPatch>,
+          ObservableCollectionStore<VertexPatch>
+        >
+    with $Provider<ObservableCollectionStore<VertexPatch>> {
+  VertexPatchStoreProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'vertexPatchStoreProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexPatchStoreHash();
+
+  @$internal
+  @override
+  $ProviderElement<ObservableCollectionStore<VertexPatch>> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ObservableCollectionStore<VertexPatch> create(Ref ref) {
+    return vertexPatchStore(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ObservableCollectionStore<VertexPatch> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride:
+          $SyncValueProvider<ObservableCollectionStore<VertexPatch>>(value),
+    );
+  }
+}
+
+String _$vertexPatchStoreHash() => r'0834a6bb89cf69b2c29c933bac23d129dbb692a8';
---
+++ vamos_cartographie/lib/core/injection/topology/queries/vertex_queries.g.dart
@@ -0,0 +1,207 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vertex_queries.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(vertexRefs)
+final vertexRefsProvider = VertexRefsProvider._();
+
+final class VertexRefsProvider
+    extends
+        $FunctionalProvider<List<VertexRef>, List<VertexRef>, List<VertexRef>>
+    with $Provider<List<VertexRef>> {
+  VertexRefsProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'vertexRefsProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexRefsHash();
+
+  @$internal
+  @override
+  $ProviderElement<List<VertexRef>> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  List<VertexRef> create(Ref ref) {
+    return vertexRefs(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<VertexRef> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<VertexRef>>(value),
+    );
+  }
+}
+
+String _$vertexRefsHash() => r'fa3d295c0747af9cd14dd37bd742b9c17de1cd1d';
+
+@ProviderFor(vertexUiElement)
+final vertexUiElementProvider = VertexUiElementFamily._();
+
+final class VertexUiElementProvider
+    extends
+        $FunctionalProvider<VertexUiElement, VertexUiElement, VertexUiElement>
+    with $Provider<VertexUiElement> {
+  VertexUiElementProvider._({
+    required VertexUiElementFamily super.from,
+    required (TripId, VertexRef) super.argument,
+  }) : super(
+         retry: null,
+         name: r'vertexUiElementProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$vertexUiElementHash();
+
+  @override
+  String toString() {
+    return r'vertexUiElementProvider'
+        ''
+        '$argument';
+  }
+
+  @$internal
+  @override
+  $ProviderElement<VertexUiElement> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  VertexUiElement create(Ref ref) {
+    final argument = this.argument as (TripId, VertexRef);
+    return vertexUiElement(ref, argument.$1, argument.$2);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(VertexUiElement value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<VertexUiElement>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is VertexUiElementProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$vertexUiElementHash() => r'655bdca08bb397e3290e44df8e47979c98bfb4e5';
+
+final class VertexUiElementFamily extends $Family
+    with $FunctionalFamilyOverride<VertexUiElement, (TripId, VertexRef)> {
+  VertexUiElementFamily._()
+    : super(
+        retry: null,
+        name: r'vertexUiElementProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  VertexUiElementProvider call(TripId tripId, VertexRef vertexRef) =>
+      VertexUiElementProvider._(argument: (tripId, vertexRef), from: this);
+
+  @override
+  String toString() => r'vertexUiElementProvider';
+}
+
+@ProviderFor(isVertexSelected)
+final isVertexSelectedProvider = IsVertexSelectedFamily._();
+
+final class IsVertexSelectedProvider
+    extends $FunctionalProvider<bool, bool, bool>
+    with $Provider<bool> {
+  IsVertexSelectedProvider._({
+    required IsVertexSelectedFamily super.from,
+    required (TripId, VertexRef) super.argument,
+  }) : super(
+         retry: null,
+         name: r'isVertexSelectedProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$isVertexSelectedHash();
+
+  @override
+  String toString() {
+    return r'isVertexSelectedProvider'
+        ''
+        '$argument';
+  }
+
+  @$internal
+  @override
+  $ProviderElement<bool> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  bool create(Ref ref) {
+    final argument = this.argument as (TripId, VertexRef);
+    return isVertexSelected(ref, argument.$1, argument.$2);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(bool value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<bool>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is IsVertexSelectedProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$isVertexSelectedHash() => r'cf6fda209e717ae768ca43e4337475918246a3ac';
+
+final class IsVertexSelectedFamily extends $Family
+    with $FunctionalFamilyOverride<bool, (TripId, VertexRef)> {
+  IsVertexSelectedFamily._()
+    : super(
+        retry: null,
+        name: r'isVertexSelectedProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  IsVertexSelectedProvider call(TripId tripId, VertexRef vertexRef) =>
+      IsVertexSelectedProvider._(argument: (tripId, vertexRef), from: this);
+
+  @override
+  String toString() => r'isVertexSelectedProvider';
+}
