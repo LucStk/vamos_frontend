5633eb117d404d06d1a44acfe9f15d29daac0f30
LucStk <luc.sterkers@laposte.net>
2026-09-18 18:50:11 +0200

    Update explore_map_screen.dart

--- apps/vamos_cartographie/lib/explore_map/explore_map_screen.dart
+++ apps/vamos_cartographie/lib/explore_map/explore_map_screen.dart
@@ -1,17 +1,9 @@
 // features/map/presentation/screens/map_page.dart
 import 'package:flutter/material.dart';
-import 'package:domain_core/domain_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:map_application/base/base.dart';
-import 'package:trip_application/trip_application.dart';
 import 'package:vamos_cartographie/base_map/base_map.dart';
 import 'package:vamos_cartographie/base_map/base_map_screen.dart';
 import 'package:vamos_cartographie/trip/injection/injection.dart';
-import 'package:vamos_cartographie/trip/injection/trip_store.dart';
-import 'package:vamos_cartographie/trip_map/editor/injection/editor_controller_provider.dart';
-import 'package:vamos_cartographie/trip_map/editor/injection/trip_editor_scene.dart';
-import 'package:vamos_cartographie/trip_map/editor/overlay_editor/map_bottom_sheet.dart';
-import 'package:vamos_cartographie/trip_map/injection/injection.dart';
 
 class ExploreMapScreen extends ConsumerWidget {
   const ExploreMapScreen({super.key});
